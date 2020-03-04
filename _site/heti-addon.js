0ca7823d8426ebc673896ef9dc978c99b579af77
Sivan <sun.sivan@gmail.com>
2020-03-04 23:08:13 +0800

    fix: add error catch for browsers dont support positive lookbehind

--- _site/heti-addon.js
+++ _site/heti-addon.js
@@ -688,6 +688,13 @@
 
 	class Heti {
 	  constructor (rootSelector) {
+	    try {
+	      new RegExp(`(?<=\d)\d`, 'g').test('');
+	    } catch (err) {
+	      console.warn(err.name, '浏览器尚未实现 RegExp positive lookbehind');
+	      return
+	    }
+
 	    this.rootSelector = rootSelector || '.heti';
 	    this.REG_FULL = new RegExp(`(?<=[${CJK}])( *[${ANS}]+(?: +[${ANS}]+)* *)(?=[${CJK}])`, 'g');
 	    this.REG_START = new RegExp(`([${ANS}]+(?: +[${ANS}]+)* *)(?=[${CJK}])`, 'g');
